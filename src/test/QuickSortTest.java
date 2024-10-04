d5fa0b7886defbb328cfa0a8128a02cd10803bc0
Leo <55858371+bellinileo@users.noreply.github.com>
2024-10-04 01:54:52 -0700

    Update QuickSortTest.java

--- src/test/QuickSortTest.java
+++ src/test/QuickSortTest.java
@@ -5,28 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuickSortTest {
 
-    @Test
-    public void testQuickSort() {
-        int[] array = {10, 7, 8, 9, 1, 5};
-        int[] expectedArray = {1, 5, 7, 8, 9, 10};
+@Test
+    void testPositiveNumbers() {
+        int[] array = {4, 2, 7, 1, 9, 3};
         QuickSort.quickSort(array, 0, array.length - 1);
-        assertArrayEquals(expectedArray, array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 7, 9}, array);
     }
 
     @Test
-    public void testQuickSortEmptyArray() {
-        int[] array = {};
-        int[] expectedArray = {};
+    void testNegativeNumbers() {
+        int[] array = {-3, -1, -7, -4, -2};
         QuickSort.quickSort(array, 0, array.length - 1);
-        assertArrayEquals(expectedArray, array);
+        assertArrayEquals(new int[]{-7, -4, -3, -2, -1}, array);
+
+@Test
+    void testPosMixNeg() {
+        int[] array = {4, -2, -7, 1, 9, -3};
+        QuickSort.quickSort(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{-7, -3, 1, 4, 7, 9}, array);
     }
 
     @Test
-    public void testQuickSortSingleElement() {
-        int[] array = {5};
-        int[] expectedArray = {5};
+    void testPreSorted() {
+        int[] array = {1, 2, 3, 4, 5};
         QuickSort.quickSort(array, 0, array.length - 1);
-        assertArrayEquals(expectedArray, array);
-    }
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
 }
 
