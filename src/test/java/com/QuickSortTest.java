c743787ece6deda134e1ad3d4f23f59b03e612b9
simon <simon@me.com>
2024-10-04 02:09:03 -0700

    typo

--- src/test/java/com/QuickSortTest.java
+++ src/test/java/com/QuickSortTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuickSortTest {
 
-@Test
+   @Test
     void testPositiveNumbers() {
         int[] array = {4, 2, 7, 1, 9, 3};
         QuickSort.quickSort(array, 0, array.length - 1);
@@ -17,9 +17,9 @@ public class QuickSortTest {
         int[] array = {-3, -1, -7, -4, -2};
         QuickSort.quickSort(array, 0, array.length - 1);
         assertArrayEquals(new int[]{-7, -4, -3, -2, -1}, array);
-
-@Test
-    void testPosMixNeg() {
+    }
+     @Test
+     void testPosMixNeg() {
         int[] array = {4, -2, -7, 1, 9, -3};
         QuickSort.quickSort(array, 0, array.length - 1);
         assertArrayEquals(new int[]{-7, -3, 1, 4, 7, 9}, array);
@@ -30,5 +30,5 @@ public class QuickSortTest {
         int[] array = {1, 2, 3, 4, 5};
         QuickSort.quickSort(array, 0, array.length - 1);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
-}
+    }
 
