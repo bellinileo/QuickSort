604c803b2967547af9227baae105d0dfdd919a38
Leo <55858371+bellinileo@users.noreply.github.com>
2024-10-04 18:23:03 -0700

    Update QuickSortTest.java

--- src/test/java/com/example/QuickSortTest.java
+++ src/test/java/com/example/QuickSortTest.java
@@ -32,14 +32,6 @@ public class QuickSortTest {
     }
     
     @Test
-    public void testQuickSortWideRange() {
-        int[] array = {-1234, 567, -890, 345, -1500, 999, -2000, 1342, 789, -456, 1000, -999, 543, -1820, 2000, -320, 1111, -654, 1357, -182, -760, 1760, 111, -1212, 1500, -111, 220, 900, -450};
-        int[] expectedArray = {-2000, -1820, -1500, -1234, -1212, -999, -890, -760, -654, -456, -450, -320, -182, 111, 220, 345, 567, 789, 900, 999, 1111, 1342, 1500, 1760, 2000};
-        QuickSort.quickSort(array, 0, array.length - 1);
-        assertArrayEquals(expectedArray, array);
-    }
-
-    @Test
     public void testQuickRepeats() {
         int[] array = {10, 10, 10, 10, -5, -5};
         int[] expectedArray = {-5, -5, 10, 10, 10, 10};
@@ -47,14 +39,7 @@ public class QuickSortTest {
         assertArrayEquals(expectedArray, array);
     }
     
-    @Test
-    public void testQuickSortNull() {
-        int[] array = null;
-        assertThrows(IllegalArgumentException.class, () -> {
-            QuickSort.quickSort(array, 0, 0); // Should throw exception
-        });
-    }
-    
+ 
     @Test
     public void testQuickSortInvalidIndi() {
         int[] array = {5, 3, 8};
